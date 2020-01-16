c5c0a43b59eb7c0466468ec48f38eda85663a456
Efraim A. Lorenzana <efraim.lorenzana@gmail.com>
2020-01-16 12:06:39 +0800

    Updates

---
+++ precache-manifest.a3d0dabcfbb740cd74c81c8466e8cc6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "977bbcca6b8461cefa7e6cdb8d6f3ae3",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "4597f155d9de8085d424",
+    "url": "/home/static/css/main.7c3ca28a.chunk.css"
+  },
+  {
+    "revision": "1482259b680daefaac24",
+    "url": "/home/static/js/2.ee15fd7e.chunk.js"
+  },
+  {
+    "revision": "0ac748c594a9fec6145054650043eb61",
+    "url": "/home/static/js/2.ee15fd7e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4597f155d9de8085d424",
+    "url": "/home/static/js/main.617fa25f.chunk.js"
+  },
+  {
+    "revision": "25f4efb79331ae3b25ab",
+    "url": "/home/static/js/runtime-main.3f2035d2.js"
+  }
+]);
